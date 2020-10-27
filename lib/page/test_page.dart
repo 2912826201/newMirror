0331821fb875623fd61e3fde97ec38e2ba1b7320
Nicholas <278873964@qq.com>
2020-10-27 15:19:40 +0800

    项目中增加图片文件夹及一张测试图

--- lib/page/test_page.dart
+++ lib/page/test_page.dart
@@ -16,7 +16,25 @@ class _TestState extends State<TestPage> {
         title: Text("测试页"),
       ),
       body: Center(
-        child: Text("测试用页面，可随意添加组件"),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("测试用页面，可随意添加组件"),
+            Image(
+              image: AssetImage("images/test.png"),
+              color: Colors.redAccent,
+              colorBlendMode: BlendMode.darken,
+              width: 100.0,
+              height: 100.0,
+            ),
+            Image(
+              image: NetworkImage(
+                  "http://i2.hdslb.com/bfs/face/c2d82a7e6512a85657e997dc8f84ab538e87a8cc.jpg"),
+              width: 100.0,
+              height: 100.0,
+            ),
+          ],
+        ),
       ),
     );
   }
