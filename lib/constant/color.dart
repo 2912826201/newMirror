6c74ffe82083199d1e92fe41f77dc9c905ad7355
Nicholas <278873964@qq.com>
2020-11-12 16:13:06 +0800

    增加了颜色常量

---
+++ lib/constant/color.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+/// color
+/// Created by yangjiayi on 2020/11/12.
+
+class AppColor {
+  //MR 主题红
+  static const mainRed = Color.fromRGBO(0xFF, 0x40, 0x59, 1.0);
+  //MB 主题蓝 @、#
+  static const mainBlue = Color.fromRGBO(0x57, 0x6B, 0x95, 1.0);
+
+  //N0 白色
+  static const white = Color.fromRGBO(0xFF, 0xFF, 0xFF, 1.0);
+
+  //N3 背景
+  static const bgBlack = Color.fromRGBO(0x16, 0x16, 0x16, 1.0);
+  //N2 背景及分割线
+  static const bgWhite = Color.fromRGBO(0xF3, 0xF3, 0xF3, 1.0);
+
+  //N8 主文字1
+  static const textPrimary1 = Color.fromRGBO(0x11, 0x11, 0x11, 1.0);
+  //N7 主文字2
+  static const textPrimary2 = Color.fromRGBO(0x33, 0x33, 0x33, 1.0);
+  //N6 主文字3
+  static const textPrimary3 = Color.fromRGBO(0x66, 0x66, 0x66, 1.0);
+  //N5 辅助、时间文字
+  static const textSecondary = Color.fromRGBO(0x99, 0x99, 0x99, 1.0);
+  //N4 Disable、提示文字
+  static const textHint = Color.fromRGBO(0xCC, 0xCC, 0xCC, 1.0);
+}
