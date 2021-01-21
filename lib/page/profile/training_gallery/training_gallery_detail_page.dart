36be913564eafb458880686dc4c4fa0f1badef83
Nicholas <278873964@qq.com>
2021-01-21 14:21:17 +0800

    开发健身相册图片详情页，目前两指放大缩小和划动上一页下一页有冲突

---
+++ lib/page/profile/training_gallery/training_gallery_detail_page.dart
@@ -0,0 +1,115 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:mirror/constant/color.dart';
+import 'package:mirror/constant/style.dart';
+import 'package:mirror/page/profile/training_gallery/training_gallery_page.dart';
+import 'package:mirror/widget/scale_view.dart';
+
+/// training_gallery_detail_page
+/// Created by yangjiayi on 2021/1/21.
+
+class TrainingGalleryDetailPage extends StatefulWidget {
+  TrainingGalleryDetailPage(this.dataList, {Key key, this.dayIndex = 0, this.imageIndex = 0}) : super(key: key);
+
+  final List<TrainingGalleryDayModel> dataList;
+  final int dayIndex;
+  final int imageIndex;
+
+  @override
+  _TrainingGalleryDetailState createState() => _TrainingGalleryDetailState();
+}
+
+class _TrainingGalleryDetailState extends State<TrainingGalleryDetailPage> {
+  List<TrainingGalleryImageModel> _imageList = [];
+  int _currentIndex = 0;
+  String _title = "";
+
+  @override
+  void initState() {
+    _initData();
+    super.initState();
+  }
+
+  _initData() {
+    for (int i = 0; i < widget.dataList.length; i++) {
+      TrainingGalleryDayModel model = widget.dataList[i];
+      for (int j = 0; j < model.images.length; j++) {
+        if (i == widget.dayIndex && j == widget.imageIndex) {
+          _currentIndex = _imageList.length;
+        }
+        _imageList.add(model.images[j]);
+      }
+    }
+    _title = "index: $_currentIndex";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColor.white,
+        brightness: Brightness.light,
+        title: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              _title,
+              style: AppStyle.textMedium18,
+            ),
+          ],
+        ),
+        leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back,
+              color: AppColor.black,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            }),
+        actions: [
+          IconButton(
+              icon: Icon(
+                Icons.more_horiz,
+                color: AppColor.black,
+              ),
+              onPressed: () {
+                print("更多");
+              }),
+        ],
+      ),
+      body: _buildBody(),
+    );
+  }
+
+  Widget _buildBody() {
+    return Container(
+      color: AppColor.white,
+      child: Swiper(
+        onIndexChanged: (index) {
+          setState(() {
+            _currentIndex = index;
+            _title = "index: $_currentIndex";
+          });
+        },
+        loop: false,
+        index: _currentIndex,
+        itemCount: _imageList.length,
+        scrollDirection: Axis.horizontal,
+        itemBuilder: _buildItem,
+      ),
+    );
+  }
+
+  Widget _buildItem(BuildContext context, int index) {
+    return ScaleView(
+      child: Center(
+        child: CachedNetworkImage(
+          imageUrl: _imageList[index].url,
+          fit: BoxFit.contain,
+        ),
+      ),
+    );
+  }
+}
--- lib/page/profile/training_gallery/training_gallery_page.dart
+++ lib/page/profile/training_gallery/training_gallery_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:mirror/constant/color.dart';
 import 'package:mirror/constant/style.dart';
 import 'package:mirror/page/media_picker/media_picker_page.dart';
+import 'package:mirror/page/profile/training_gallery/training_gallery_detail_page.dart';
 import 'package:mirror/route/router.dart';
 import 'package:mirror/util/screen_util.dart';
 
@@ -16,58 +17,86 @@ class TrainingGalleryPage extends StatefulWidget {
 }
 
 class _TrainingGalleryState extends State<TrainingGalleryPage> {
-  List<_TrainingGalleryDayModel> _dataList = [];
+  List<TrainingGalleryDayModel> _dataList = [];
 
   _initData() {
-    _dataList.add(_TrainingGalleryDayModel()
+    _dataList.add(TrainingGalleryDayModel()
       ..month = "1月 2021"
       ..day = "18"
       ..images = [
-        "https://i2.hdslb.com/bfs/archive/4431155151f0b9b3da5f150b7b4273bcc525afe1.jpg",
-        "https://i2.hdslb.com/bfs/archive/2c7f0d9854199fa61ffeb06af05f3c81fd30e19d.jpg",
-        "https://i0.hdslb.com/bfs/archive/a4cd4bf40a10d6d1a841ab4246db86d8df713c64.jpg",
-        "https://i1.hdslb.com/bfs/archive/08a1ac97644b93563cd17a601a81803528133f71.jpg",
-        "https://i2.hdslb.com/bfs/archive/857d28d02e35ef9eefcdfea0b8b00d608a08e682.jpg",
-        "https://i2.hdslb.com/bfs/archive/de2b17a1e96a26c3b8222b316f8417fba45c7737.jpg",
-        "https://i1.hdslb.com/bfs/archive/7bd977dacfe35cecb5ea13df2673bb453ea53195.jpg"
+        TrainingGalleryImageModel()
+          ..url = "https://i2.hdslb.com/bfs/archive/4431155151f0b9b3da5f150b7b4273bcc525afe1.jpg",
+        TrainingGalleryImageModel()
+          ..url = "https://i2.hdslb.com/bfs/archive/2c7f0d9854199fa61ffeb06af05f3c81fd30e19d.jpg",
+        TrainingGalleryImageModel()
+          ..url = "https://i0.hdslb.com/bfs/archive/a4cd4bf40a10d6d1a841ab4246db86d8df713c64.jpg",
+        TrainingGalleryImageModel()
+          ..url = "https://i1.hdslb.com/bfs/archive/08a1ac97644b93563cd17a601a81803528133f71.jpg",
+        TrainingGalleryImageModel()
+          ..url = "https://i2.hdslb.com/bfs/archive/857d28d02e35ef9eefcdfea0b8b00d608a08e682.jpg",
+        TrainingGalleryImageModel()
+          ..url = "https://i2.hdslb.com/bfs/archive/de2b17a1e96a26c3b8222b316f8417fba45c7737.jpg",
+        TrainingGalleryImageModel()
+          ..url = "https://i1.hdslb.com/bfs/archive/7bd977dacfe35cecb5ea13df2673bb453ea53195.jpg"
       ]);
-    _dataList.add(_TrainingGalleryDayModel()
+    _dataList.add(TrainingGalleryDayModel()
       ..month = "1月 2021"
       ..day = "10"
-      ..images = ["https://i2.hdslb.com/bfs/archive/de2b17a1e96a26c3b8222b316f8417fba45c7737.jpg"]);
-    _dataList.add(_TrainingGalleryDayModel()
+      ..images = [
+        TrainingGalleryImageModel()
+          ..url = "https://i0.hdslb.com/bfs/archive/2b8bf21ed1117b345aa596f34b6c53c310a801cb.jpg"
+      ]);
+    _dataList.add(TrainingGalleryDayModel()
       ..month = "1月 2021"
       ..day = "1"
       ..images = [
-        "https://i2.hdslb.com/bfs/archive/6dad8b9612336e1c9fd88dd54a7eac08909e410e.jpg",
-        "https://i0.hdslb.com/bfs/archive/8e58184f1dca7e82cc801d3dc170b98ec7333a42.jpg",
-        "https://i2.hdslb.com/bfs/archive/53b58742ebb860bd0ada1e9fd048c48bb8383c51.jpg",
-        "https://i2.hdslb.com/bfs/archive/76f307eab11e961ad92ced6de85f85d8d600c9f3.jpg"
+        TrainingGalleryImageModel()
+          ..url = "https://i2.hdslb.com/bfs/archive/6dad8b9612336e1c9fd88dd54a7eac08909e410e.jpg",
+        TrainingGalleryImageModel()
+          ..url = "https://i0.hdslb.com/bfs/archive/8e58184f1dca7e82cc801d3dc170b98ec7333a42.jpg",
+        TrainingGalleryImageModel()
+          ..url = "https://i2.hdslb.com/bfs/archive/53b58742ebb860bd0ada1e9fd048c48bb8383c51.jpg",
+        TrainingGalleryImageModel()
+          ..url = "https://i2.hdslb.com/bfs/archive/76f307eab11e961ad92ced6de85f85d8d600c9f3.jpg"
       ]);
-    _dataList.add(_TrainingGalleryDayModel()
+    _dataList.add(TrainingGalleryDayModel()
       ..month = "11月 2020"
       ..day = "11"
       ..images = [
-        "https://i2.hdslb.com/bfs/archive/4431155151f0b9b3da5f150b7b4273bcc525afe1.jpg",
-        "https://i2.hdslb.com/bfs/archive/2c7f0d9854199fa61ffeb06af05f3c81fd30e19d.jpg",
-        "https://i0.hdslb.com/bfs/archive/a4cd4bf40a10d6d1a841ab4246db86d8df713c64.jpg",
-        "https://i1.hdslb.com/bfs/archive/08a1ac97644b93563cd17a601a81803528133f71.jpg",
-        "https://i2.hdslb.com/bfs/archive/857d28d02e35ef9eefcdfea0b8b00d608a08e682.jpg",
-        "https://i2.hdslb.com/bfs/archive/de2b17a1e96a26c3b8222b316f8417fba45c7737.jpg",
-        "https://i1.hdslb.com/bfs/archive/7bd977dacfe35cecb5ea13df2673bb453ea53195.jpg"
+        TrainingGalleryImageModel()
+          ..url = "https://i2.hdslb.com/bfs/archive/4431155151f0b9b3da5f150b7b4273bcc525afe1.jpg",
+        TrainingGalleryImageModel()
+          ..url = "https://i2.hdslb.com/bfs/archive/2c7f0d9854199fa61ffeb06af05f3c81fd30e19d.jpg",
+        TrainingGalleryImageModel()
+          ..url = "https://i0.hdslb.com/bfs/archive/a4cd4bf40a10d6d1a841ab4246db86d8df713c64.jpg",
+        TrainingGalleryImageModel()
+          ..url = "https://i1.hdslb.com/bfs/archive/08a1ac97644b93563cd17a601a81803528133f71.jpg",
+        TrainingGalleryImageModel()
+          ..url = "https://i2.hdslb.com/bfs/archive/857d28d02e35ef9eefcdfea0b8b00d608a08e682.jpg",
+        TrainingGalleryImageModel()
+          ..url = "https://i2.hdslb.com/bfs/archive/de2b17a1e96a26c3b8222b316f8417fba45c7737.jpg",
+        TrainingGalleryImageModel()
+          ..url = "https://i1.hdslb.com/bfs/archive/7bd977dacfe35cecb5ea13df2673bb453ea53195.jpg"
       ]);
-    _dataList.add(_TrainingGalleryDayModel()
+    _dataList.add(TrainingGalleryDayModel()
       ..month = "11月 2020"
       ..day = "15"
-      ..images = ["https://i2.hdslb.com/bfs/archive/de2b17a1e96a26c3b8222b316f8417fba45c7737.jpg"]);
-    _dataList.add(_TrainingGalleryDayModel()
+      ..images = [
+        TrainingGalleryImageModel()
+          ..url = "https://i0.hdslb.com/bfs/archive/2b8bf21ed1117b345aa596f34b6c53c310a801cb.jpg"
+      ]);
+    _dataList.add(TrainingGalleryDayModel()
       ..month = "10月 2020"
       ..day = "20"
       ..images = [
-        "https://i2.hdslb.com/bfs/archive/6dad8b9612336e1c9fd88dd54a7eac08909e410e.jpg",
-        "https://i0.hdslb.com/bfs/archive/8e58184f1dca7e82cc801d3dc170b98ec7333a42.jpg",
-        "https://i2.hdslb.com/bfs/archive/53b58742ebb860bd0ada1e9fd048c48bb8383c51.jpg",
-        "https://i2.hdslb.com/bfs/archive/76f307eab11e961ad92ced6de85f85d8d600c9f3.jpg"
+        TrainingGalleryImageModel()
+          ..url = "https://i2.hdslb.com/bfs/archive/6dad8b9612336e1c9fd88dd54a7eac08909e410e.jpg",
+        TrainingGalleryImageModel()
+          ..url = "https://i0.hdslb.com/bfs/archive/8e58184f1dca7e82cc801d3dc170b98ec7333a42.jpg",
+        TrainingGalleryImageModel()
+          ..url = "https://i2.hdslb.com/bfs/archive/53b58742ebb860bd0ada1e9fd048c48bb8383c51.jpg",
+        TrainingGalleryImageModel()
+          ..url = "https://i2.hdslb.com/bfs/archive/76f307eab11e961ad92ced6de85f85d8d600c9f3.jpg"
       ]);
 
     SuspensionUtil.setShowSuspensionStatus(_dataList);
@@ -125,59 +154,65 @@ class _TrainingGalleryState extends State<TrainingGalleryPage> {
 
   Widget _buildBody() {
     if (_dataList.isEmpty) {
-      return Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              height: 224,
-              width: 224,
-              color: AppColor.bgWhite,
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            Text(
-              "还没有照片",
-              style: AppStyle.textSecondaryRegular14,
-            )
-          ],
+      return Container(
+        color: AppColor.white,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 224,
+                width: 224,
+                color: AppColor.bgWhite,
+              ),
+              SizedBox(
+                height: 16,
+              ),
+              Text(
+                "还没有照片",
+                style: AppStyle.textSecondaryRegular14,
+              )
+            ],
+          ),
         ),
       );
     } else {
-      return Column(
-        children: [
-          Expanded(
-              child: AzListView(
-            padding: EdgeInsets.zero,
-            itemBuilder: _buildItem,
-            susItemBuilder: _buildHeader,
-            itemCount: _dataList.length,
-            data: _dataList,
-            indexBarData: [],
-          )),
-          GestureDetector(
-            onTap: () {
-              print("制作对比图");
-            },
-            child: Container(
-              padding: EdgeInsets.only(bottom: ScreenUtil.instance.bottomBarHeight),
-              height: 48 + ScreenUtil.instance.bottomBarHeight,
-              color: AppColor.textPrimary2,
-              alignment: Alignment.center,
-              child: Text(
-                "制作对比图",
-                style: AppStyle.whiteRegular16,
+      return Container(
+        color: AppColor.white,
+        child: Column(
+          children: [
+            Expanded(
+                child: AzListView(
+              padding: EdgeInsets.zero,
+              itemBuilder: _buildItem,
+              susItemBuilder: _buildHeader,
+              itemCount: _dataList.length,
+              data: _dataList,
+              indexBarData: [],
+            )),
+            GestureDetector(
+              onTap: () {
+                print("制作对比图");
+              },
+              child: Container(
+                padding: EdgeInsets.only(bottom: ScreenUtil.instance.bottomBarHeight),
+                height: 48 + ScreenUtil.instance.bottomBarHeight,
+                color: AppColor.textPrimary2,
+                alignment: Alignment.center,
+                child: Text(
+                  "制作对比图",
+                  style: AppStyle.whiteRegular16,
+                ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       );
     }
   }
 
   Widget _buildHeader(BuildContext context, int index) {
-    _TrainingGalleryDayModel model = _dataList[index];
+    TrainingGalleryDayModel model = _dataList[index];
     return Container(
       height: 48,
       width: ScreenUtil.instance.screenWidthDp,
@@ -191,10 +226,10 @@ class _TrainingGalleryState extends State<TrainingGalleryPage> {
     );
   }
 
-  Widget _buildItem(BuildContext context, int index) {
-    _TrainingGalleryDayModel model = _dataList[index];
+  Widget _buildItem(BuildContext context, int dayIndex) {
+    TrainingGalleryDayModel model = _dataList[dayIndex];
     bool isLastDayInMonth = false;
-    if (index < _dataList.length - 1 && _dataList[index + 1].month != model.month) {
+    if (dayIndex < _dataList.length - 1 && _dataList[dayIndex + 1].month != model.month) {
       isLastDayInMonth = true;
     }
     return Container(
@@ -221,7 +256,7 @@ class _TrainingGalleryState extends State<TrainingGalleryPage> {
                   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                       childAspectRatio: 1, crossAxisCount: 3, crossAxisSpacing: 3, mainAxisSpacing: 3),
                   itemBuilder: (context, index) {
-                    return _buildImage(context, index, model.images[index]);
+                    return _buildImage(context, index, dayIndex);
                   }),
             ),
           ),
@@ -230,20 +265,37 @@ class _TrainingGalleryState extends State<TrainingGalleryPage> {
     );
   }
 
-  Widget _buildImage(BuildContext context, int index, String url) {
-    return CachedNetworkImage(
-      imageUrl: url,
-      fit: BoxFit.cover,
+  Widget _buildImage(BuildContext context, int index, int dayIndex) {
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return TrainingGalleryDetailPage(
+            _dataList,
+            dayIndex: dayIndex,
+            imageIndex: index,
+          );
+        }));
+      },
+      child: CachedNetworkImage(
+        imageUrl: _dataList[dayIndex].images[index].url,
+        fit: BoxFit.cover,
+      ),
     );
   }
 }
 
 // 要继承这个ISuspensionBean
-class _TrainingGalleryDayModel with ISuspensionBean {
+class TrainingGalleryDayModel with ISuspensionBean {
   String month;
   String day;
-  List<String> images;
+  List<TrainingGalleryImageModel> images;
 
   @override
   String getSuspensionTag() => month;
+}
+
+class TrainingGalleryImageModel {
+  int id;
+  String url;
+  int createTime;
 }
