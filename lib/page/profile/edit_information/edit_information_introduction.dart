3020ea917e19e1d6635338bf5084ae0fae16f23d
sl <344238281@qq.com>
2021-02-01 11:56:33 +0800

    1、发布动态键盘弹起修改@和话题高度。2、修改个人主页编辑资料输入框在ios上的bug。

--- lib/page/profile/edit_information/edit_information_introduction.dart
+++ lib/page/profile/edit_information/edit_information_introduction.dart
@@ -2,84 +2,108 @@ import 'package:flutter/material.dart';
 import 'package:mirror/constant/color.dart';
 import 'package:mirror/constant/style.dart';
 import 'package:mirror/util/screen_util.dart';
+import 'package:mirror/widget/Input_method_rules/pin_yin_text_edit_controller.dart';
 
-class EditInformationIntroduction extends StatefulWidget{
+class EditInformationIntroduction extends StatefulWidget {
   final String introduction;
+
   EditInformationIntroduction({this.introduction});
 
   @override
   _IntroductionState createState() {
-   return _IntroductionState();
+    return _IntroductionState();
   }
-
 }
-class _IntroductionState extends State<EditInformationIntroduction>{
+
+class _IntroductionState extends State<EditInformationIntroduction> {
   //同步的输入框和上个界面带过来的简介
   String editText;
+
   //底部的提示int
   int textLength = 0;
   double textHeight;
-  FocusNode blankNode = FocusNode();
+  ///记录上次结果
+  var lastInput = "";
+  PinYinTextEditController controller = PinYinTextEditController();
   @override
   void dispose() {
     // TODO: implement dispose
     super.dispose();
-   /* blankNode.unfocus();*/
   }
+
   @override
   void initState() {
     super.initState();
     //先同步简介
-    if(widget.introduction==null||widget.introduction=="去编辑"){
+    if (widget.introduction == null || widget.introduction == "去编辑") {
       editText = "";
-    }else{
+    } else {
       editText = widget.introduction;
       textLength = widget.introduction.length;
+      controller.text = editText;
+      // 设置光标
+      var setCursor = TextSelection(
+        baseOffset: controller.text.length,
+        extentOffset: controller.text.length,
+      );
+      controller.selection = setCursor;
     }
+    controller.addListener(() {
+      if (lastInput != controller.completeText) {
+        lastInput = controller.completeText;
+        ///通知onChanged
+        setState(() {
+          textLength = lastInput.length;
+          editText = lastInput;
+        });
+      }
+    });
   }
+
   @override
   Widget build(BuildContext context) {
     double width = ScreenUtil.instance.screenWidthDp;
     double height = ScreenUtil.instance.height;
-          return Scaffold(
-            appBar: AppBar(
-            backgroundColor: AppColor.white,
-            leading:InkWell(
-              child: Container(
-                margin: EdgeInsets.only(left: 16),
-                child: Image.asset("images/resource/2.0x/return2x.png"),),
-              onTap: (){
-                FocusScope.of(context).requestFocus(blankNode);
-                Navigator.pop(context,widget.introduction);
-              },
+    return Scaffold(
+      appBar: AppBar(
+          backgroundColor: AppColor.white,
+          leading: InkWell(
+            child: Container(
+              margin: EdgeInsets.only(left: 16),
+              child: Image.asset("images/resource/2.0x/return2x.png"),
             ),
-              leadingWidth: 44,
-            title: Text("编辑简介",style: AppStyle.textMedium18,),
-            centerTitle: true,
-            actions: [
-              InkWell(
-                onTap: (){
-                  FocusScope.of(context).requestFocus(blankNode);
-                  Navigator.pop(this.context,editText);
+            onTap: () {
+              Navigator.pop(context, widget.introduction);
+            },
+          ),
+          leadingWidth: 44,
+          title: Text(
+            "编辑简介",
+            style: AppStyle.textMedium18,
+          ),
+          centerTitle: true,
+          actions: [
+            InkWell(
+                onTap: () {
+                  Navigator.pop(this.context, editText);
                 },
                 child: Container(
-                width: 60,
-                margin: EdgeInsets.only(right: 16),
-                child: Center(
-                  child:Container(
+                  width: 60,
+                  margin: EdgeInsets.only(right: 16),
+                  child: Center(
+                      child: Container(
                     decoration: BoxDecoration(
                       color: AppColor.mainRed,
                       borderRadius: BorderRadius.all(Radius.circular(14)),
                     ),
-                    padding: EdgeInsets.only(left:16 ,right:16 ,top: 4,bottom:4 ),
+                    padding: EdgeInsets.only(left: 16, right: 16, top: 4, bottom: 4),
                     child: Text(
                       "确定",
                       style: TextStyle(fontSize: 14, color: AppColor.white),
-                    ),)
-                ),
-              )),
-              ]
-            ),
+                    ),
+                  )),
+                )),
+          ]),
       body: Container(
         color: AppColor.white,
         height: height - ScreenUtil.instance.statusBarHeight,
@@ -102,47 +126,40 @@ class _IntroductionState extends State<EditInformationIntroduction>{
   }
 
   //输入框
-  Widget _inputBox(double width,double height){
+  Widget _inputBox(double width, double height) {
     return Container(
       height: 148,
       width: width,
-      margin: EdgeInsets.only(left: 16,right: 16),
-      padding: EdgeInsets.only(left: 16,right: 16,top: 8),
+      margin: EdgeInsets.only(left: 16, right: 16),
+      padding: EdgeInsets.only(left: 16, right: 16, top: 8),
       decoration: BoxDecoration(
-        borderRadius: BorderRadius.all(Radius.circular(8)),
-        border: Border.all(width: 0.5, color: AppColor.bgWhite)),
-        child: Column(
-          children: [
+          borderRadius: BorderRadius.all(Radius.circular(8)), border: Border.all(width: 0.5, color: AppColor.bgWhite)),
+      child: Column(
+        children: [
           TextField(
-          maxLength: 90,
-          maxLines: 5,
-          cursorColor:AppColor.black,
-          style: AppStyle.textRegular16,
+            autofocus: true,
+            maxLength: 90,
+            maxLines: 5,
+            cursorColor: AppColor.black,
+            style: AppStyle.textRegular16,
             //初始化值，设置光标始终在最后
-            controller: TextEditingController.fromValue(TextEditingValue(
-              text: editText,
-              selection: TextSelection.fromPosition(TextPosition(
-                affinity: TextAffinity.downstream,
-                offset: editText.length))
-            )),
-          decoration: InputDecoration(
-            counterText: '',
-            hintText: "有意思的简介会吸引更多关注~",
-            hintStyle:TextStyle(fontSize: 16,color: AppColor.textHint),
-            border: InputBorder.none,),
-          onChanged: (value){
-            setState(() {
-              textLength = value.length;
-              editText = value;
-            });
-          },
-        ),
-            Container(
-              alignment: Alignment.bottomRight,
-              child: Text("$textLength/90",style: AppStyle.textHintRegular12,),
-            )
-          ],
-        ),
+            controller: controller,
+            decoration: InputDecoration(
+              counterText: '',
+              hintText: "有意思的简介会吸引更多关注~",
+              hintStyle: TextStyle(fontSize: 16, color: AppColor.textHint),
+              border: InputBorder.none,
+            ),
+          ),
+          Container(
+            alignment: Alignment.bottomRight,
+            child: Text(
+              "$textLength/90",
+              style: AppStyle.textHintRegular12,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
