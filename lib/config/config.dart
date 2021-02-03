09768d803c2389910d2451d530f231329b30ad7e
[lianxin] <[lianxin@aimymusic.com]>
2021-02-03 11:06:40 +0800

    更新apk跳转安装，ios跳转商店

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -103,6 +103,10 @@ class AppConfig {
   static String getAppVoiceDir() {
     return "$_appDir/voice";
   }
+  //获取语音文件的路径
+  static String getAppApkDir() {
+    return "$_appDir/apk";
+  }
 
   static String getAppVoiceFilePath() {
     return "${getAppVoiceDir()}/record_${new DateTime.now().millisecondsSinceEpoch}.aac";
