306c6141c1911d1ddbc28e49d9e4dc75f92589a4
Nicholas <278873964@qq.com>
2021-02-07 15:41:59 +0800

    新增响应用户从未登录到登录状态时的处理

--- lib/page/profile/login_test_page.dart
+++ lib/page/profile/login_test_page.dart
@@ -16,7 +16,20 @@ import 'package:provider/provider.dart';
 
 import 'package:mirror/route/router.dart';
 
-class LoginTestPage extends StatelessWidget {
+class LoginTestPage extends StatefulWidget {
+  @override
+  _LoginTestState createState() => _LoginTestState();
+}
+
+class _LoginTestState extends State<LoginTestPage> {
+  bool isLogin = false;
+
+  @override
+  void initState() {
+    super.initState();
+    isLogin = context.read<TokenNotifier>().isLoggedIn;
+  }
+
   @override
   Widget build(BuildContext context) {
     print("登录测试页");
@@ -26,6 +39,16 @@ class LoginTestPage extends StatelessWidget {
       ),
       body: Center(
         child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+          Consumer<TokenNotifier>(
+            builder: (context, notifier, child) {
+              if (isLogin == false && notifier.isLoggedIn == true) {
+                print("执行用户完成登录后的操作");
+              }
+              isLogin = notifier.isLoggedIn;
+              return child;
+            },
+            child: Container(),
+          ),
           FlatButton(
             child: Text("去登录"),
             onPressed: () {
