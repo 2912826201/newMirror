7208758940485a613537a6b9d27d37ddcfaedad1
Nicholas <278873964@qq.com>
2021-02-07 18:17:28 +0800

    首页将TabBar写到AppBar中

--- lib/page/home/home_page.dart
+++ lib/page/home/home_page.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:mirror/constant/color.dart';
 import 'package:mirror/data/notifier/feed_notifier.dart';
 import 'package:mirror/page/home/sub_page/attention_page.dart';
-import 'package:mirror/page/home/sub_page/home_top_tab.dart';
 import 'package:mirror/page/home/sub_page/recommend_page.dart';
-import 'package:mirror/util/screen_util.dart';
+import 'package:mirror/page/media_picker/media_picker_page.dart';
+import 'package:mirror/page/search/search.dart';
+import 'package:mirror/route/router.dart';
+import 'package:mirror/widget/custom_appbar.dart';
+import 'package:mirror/widget/round_underline_tab_indicator.dart';
 import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
-  HomePage({
-    Key key,
-    this.controller
-  }) : super(key: key);
+  HomePage({Key key, this.controller}) : super(key: key);
   TabController controller;
 
   HomePageState createState() => HomePageState(controller: controller);
@@ -21,6 +22,7 @@ class HomePageState extends State<HomePage> with AutomaticKeepAliveClientMixin, 
 
   // taBar和TabBarView必要的
   TabController controller;
+
   @override
   bool get wantKeepAlive => true; //必须重写
 
@@ -29,66 +31,64 @@ class HomePageState extends State<HomePage> with AutomaticKeepAliveClientMixin, 
     super.initState();
     controller = TabController(length: 2, vsync: this, initialIndex: 1);
   }
+
   @override
   void dispose() {
     controller.dispose();
     super.dispose();
   }
 
-
-
-    @override
-    Widget build(BuildContext context) {
-      print("HomePage_____________________________________________build");
-      // print("首页");
-      if(context.watch<FeedMapNotifier>().postFeedModel != null) {
-        controller.index = 0;
-      }
-      // pulishFeed();
-      return Container(
-        child: Stack(
-          children: [
-            Container(
-              // 设置背景色
-              decoration: BoxDecoration(color: Colors.white),
-              // 层叠布局
-              child: Stack(
-                // 子布局定位
-                children: [
-                  Positioned(
-                      top: ScreenUtil.instance.statusBarHeight,
-                      height: 44,
-                      width: ScreenUtil.instance.screenWidthDp,
-                      child: Container(
-                        child: HomeTopTab(controller: controller,),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          border: Border(bottom: BorderSide(width: 0.5, color: Color(0xffe5e5e5))),
-                        ),
-                      )),
-                  Container(
-                    margin: EdgeInsets.only(top: 44 + ScreenUtil.instance.statusBarHeight),
-                    // child: Column(
-                    //   children: [
-                         // createdPostPromptView(),
-                    child:TabBarView(
-                          controller: this.controller,
-                          children: [
-                              AttentionPage(
-                                postFeedModel: context.watch<FeedMapNotifier>().postFeedModel,
-                            ),
-                            RecommendPage(
-                            )
-                            // RecommendPage()
-                          ],
-                        ),
-                  )
-                ],
-              ),
-            ),
-          ],
-        ),
-      );
-      // return
+  @override
+  Widget build(BuildContext context) {
+    print("HomePage_____________________________________________build");
+    // print("首页");
+    if (context.watch<FeedMapNotifier>().postFeedModel != null) {
+      controller.index = 0;
     }
+    return Scaffold(
+      appBar: CustomAppBar(
+        leading: CustomAppBarIconButton(Icons.camera_alt_outlined, AppColor.black, true, () {
+          AppRouter.navigateToMediaPickerPage(context, 9, typeImageAndVideo, true, startPageGallery, false, (result) {},
+              publishMode: 1);
+        }),
+        titleWidget: Container(
+          width: 140,
+          child: TabBar(
+            controller: controller,
+            tabs: [Text("关注"), Text("推荐")],
+            labelStyle: TextStyle(fontSize: 18),
+            labelColor: Colors.black,
+            // indicatorPadding: EdgeInsets.symmetric(horizontal: 24),
+            // unselectedLabelColor: Color.fromRGBO(153, 153, 153, 1),
+            unselectedLabelStyle: TextStyle(fontSize: 16),
+            indicator: RoundUnderlineTabIndicator(
+              borderSide: BorderSide(
+                width: 3,
+                color: Color.fromRGBO(253, 137, 140, 1),
+              ),
+              insets: EdgeInsets.only(bottom: -6),
+              wantWidth: 16,
+            ),
+          ),
+        ),
+        actions: [
+          CustomAppBarIconButton(Icons.search, AppColor.black, false, () {
+            Navigator.push(context, MaterialPageRoute(builder: (context) {
+              return SearchPage();
+            }));
+          }),
+        ],
+      ),
+      body: TabBarView(
+        controller: this.controller,
+        children: [
+          AttentionPage(
+            postFeedModel: context.watch<FeedMapNotifier>().postFeedModel,
+          ),
+          RecommendPage()
+          // RecommendPage()
+        ],
+      ),
+    );
   }
+}
