2994cd6473f6fb0b5f5e5c94783afcaa88285828
e <111>
2021-02-22 11:03:48 +0800

    提交注释

--- lib/page/message/chat_page.dart
+++ lib/page/message/chat_page.dart
@@ -319,20 +319,7 @@ class ChatPageState extends State<ChatPage> with TickerProviderStateMixin {
   //获取appbar
   Widget getAppBar() {
     return CustomAppBar(
-      titleWidget: GestureDetector(
-        child: Text(
-          // chatUserName + "-" + chatType,
-          chatUserName??"",
-          maxLines: 1,
-          overflow: TextOverflow.ellipsis,
-        ),
-        onTap: () {
-          // Message msg = chatDataList[chatDataList.length - 2].msg;
-          // AtMsg atMsg = new AtMsg(groupId: int.parse(msg.targetId), sendTime: msg.sentTime, messageUId: msg.messageUId);
-          // Application.atMesGroupModel.add(atMsg);
-          // jumpPage(KeyboardSensitiveView(),false,context);
-        },
-      ),
+      titleString: chatUserName??"",
       actions: [
         CustomAppBarIconButton(Icons.more_horiz, AppColor.black, false, _topMoreBtnClick),
       ],
@@ -732,6 +719,8 @@ class ChatPageState extends State<ChatPage> with TickerProviderStateMixin {
     if (chatTypeId == RCConversationType.Group) {
       getChatGroupUserModelList(chatUserId, context);
     }
+    
+    print("----------------------------chatUserName:$chatUserName");
   }
 
   //初始化一些数据
