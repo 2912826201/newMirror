17721510ff90c29fd590924d11d346e36053e0c4
sl <344238281@qq.com>
2021-02-25 17:05:08 +0800

    1、修改禅道bug6344

--- lib/data/model/home/feed_laud_list.dart
+++ lib/data/model/home/feed_laud_list.dart
@@ -4,13 +4,15 @@ class FeedLaudListModel {
   int feedId;
   int laudTime;
   String description;
-  FeedLaudListModel({ this.uid,this.feedId,this.avatarUrl,this.laudTime,this.description});
+  String nickName;
+  FeedLaudListModel({ this.uid,this.feedId,this.avatarUrl,this.laudTime,this.description,this.nickName});
   FeedLaudListModel.fromJson(Map<String, dynamic> json) {
     uid = json["uid"];
     avatarUrl = json["avatarUrl"];
     feedId = json["feedId"];
     laudTime = json["laudTime"];
     description = json["description"];
+    nickName = json["nickName"];
   }
   Map<String, dynamic> toJson() {
     var map = <String, dynamic>{};
@@ -19,6 +21,7 @@ class FeedLaudListModel {
     map["feedId"] = feedId;
     map["laudTime"] = laudTime;
     map["description"] = description;
+    map["nickName"] = nickName;
     return map;
   }
 }
