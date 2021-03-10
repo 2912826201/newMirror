198933cfad21573a24060ba4a4552dcf0d3a64aa
Nicholas <278873964@qq.com>
2021-03-10 18:42:28 +0800

    添加3个图标，修复icon颜色错误，替换按压半透明的实现方式

--- lib/widget/icon.dart
+++ lib/widget/icon.dart
@@ -8,13 +8,17 @@ import 'package:mirror/constant/color.dart';
 class AppIcon {
   static const String nav_return = "assets/svg/nav_return.svg";
   static const String nav_close = "assets/svg/nav_close.svg";
-  static const String qrcode_scan = "assets/svg/qrcode_scan.svg";
-  static const String machine_connected = "assets/svg/machine_connected.svg";
-  static const String machine_disconnected = "assets/svg/machine_disconnected.svg";
   static const String login_phone = "assets/svg/login_phone.svg";
   static const String login_apple = "assets/svg/login_apple.svg";
   static const String login_qq = "assets/svg/login_qq.svg";
   static const String login_wechat = "assets/svg/login_wechat.svg";
+  static const String add_circle_black = "assets/svg/add_circle_black.svg";
+  static const String clear_circle_grey = "assets/svg/clear_circle_grey.svg";
+  static const String arrow_right = "assets/svg/arrow_right.svg";
+
+  static const String qrcode_scan = "assets/svg/qrcode_scan.svg";
+  static const String machine_connected = "assets/svg/machine_connected.svg";
+  static const String machine_disconnected = "assets/svg/machine_disconnected.svg";
 
   static Widget getAppIcon(String svgName, double iconSize,
       {double containerHeight, double containerWidth, Color color, Color bgColor, bool isCircle}) {
@@ -24,11 +28,8 @@ class AppIcon {
     if (containerWidth == null) {
       containerWidth = iconSize;
     }
-    if (color == null) {
-      color = AppColor.black;
-    }
     if (bgColor == null) {
-      color = AppColor.transparent;
+      bgColor = AppColor.transparent;
     }
     if (isCircle == null) {
       isCircle = false;
@@ -91,7 +92,14 @@ class _AppIconButtonState extends State<AppIconButton> {
   Widget build(BuildContext context) {
     return GestureDetector(
       behavior: HitTestBehavior.opaque,
-      child: widget.icon != null ? _buildIcon() : _buildAppIcon(),
+      child: isPressed
+          ? Opacity(
+              opacity: 0.5,
+              child: widget.icon != null ? _buildIcon() : _buildAppIcon(),
+            )
+          : widget.icon != null
+              ? _buildIcon()
+              : _buildAppIcon(),
       onTapDown: (details) {
         setState(() {
           isPressed = true;
@@ -117,19 +125,17 @@ class _AppIconButtonState extends State<AppIconButton> {
       height: widget.buttonHeight == null ? widget.iconSize : widget.buttonHeight,
       color: widget.isCircle
           ? null
-          : isPressed
-              ? widget.bgColor.withOpacity(0.5)
-              : widget.bgColor,
+          : widget.bgColor,
       decoration: widget.isCircle
           ? BoxDecoration(
-              color: isPressed ? widget.bgColor.withOpacity(0.5) : widget.bgColor,
+              color: widget.bgColor,
               shape: BoxShape.circle,
             )
           : null,
       alignment: Alignment.center,
       child: Icon(
         widget.icon,
-        color: isPressed ? widget.iconColor.withOpacity(0.5) : widget.iconColor,
+        color: widget.iconColor,
         size: widget.iconSize,
       ),
     );
@@ -141,8 +147,8 @@ class _AppIconButtonState extends State<AppIconButton> {
       widget.iconSize,
       containerHeight: widget.buttonHeight == null ? widget.iconSize : widget.buttonHeight,
       containerWidth: widget.buttonWidth == null ? widget.iconSize : widget.buttonWidth,
-      color: isPressed ? widget.iconColor.withOpacity(0.5) : widget.iconColor,
-      bgColor: isPressed ? widget.bgColor.withOpacity(0.5) : widget.bgColor,
+      color: widget.iconColor,
+      bgColor: widget.bgColor,
       isCircle: widget.isCircle,
     );
   }
