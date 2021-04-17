a8514cfdecc2a790cacb9c210de27164a56d5855
e <111>
2021-04-17 17:02:23 +0800

    暂时隐藏处理删除国旗的的方法

--- lib/widget/feed/release_feed_input_formatter.dart
+++ lib/widget/feed/release_feed_input_formatter.dart
@@ -366,33 +366,37 @@ class ReleaseFeedInputFormatter extends TextInputFormatter {
         composing: TextRange.empty,
       );
     }else{
-        ///这是多选删除
-        if(oldValue.text.characters.length-newValue.text.characters.length>1){
-          return newValue;
-        }
-        ///这是单字符删除
-        print('------------------------------删除监听${newValue.selection.baseOffset}---${oldValue.selection.baseOffset}');
-        String backText = "";
-        String beginText = "";
-        String lastText = "";
-        try{
-          /*note 这里只处理以characters为单位的单个字符，已知删除前的光标必为老值的光标，删除后的光标不能用新值的光标(默认没删完)
-          所以用老值的光标取值再去掉最后一位，可得到删除的内容*/
 
-          beginText = oldValue.text.substring(0,oldValue.selection.baseOffset);
-          beginText  = beginText.characters.getRange(0,beginText.characters
-              .length-1).string;
-          lastText = oldValue.text.characters.getRange(beginText.characters.length+1,oldValue.text.characters.length).string;
-          backText = beginText+lastText;
-        }catch(e){
-          print('--------------------------------$e');
-      }
-        return TextEditingValue(
-            text:backText,
-            selection: TextSelection(
-              baseOffset: beginText.length,
-              extentOffset:  beginText.length,
-            ));
+      return newValue;
+        ///这是多选删除
+      //fixme 这里是处理删除国旗时可能会出现删除半个出现文字的bug
+      //fixme 但是会在ios上出现删除普通的文字出现问题
+      //   if(oldValue.text.characters.length-newValue.text.characters.length>1){
+      //     return newValue;
+      //   }
+      //   ///这是单字符删除
+      //   print('------------------------------删除监听${newValue.selection.baseOffset}---${oldValue.selection.baseOffset}');
+      //   String backText = "";
+      //   String beginText = "";
+      //   String lastText = "";
+      //   try{
+      //     /*note 这里只处理以characters为单位的单个字符，已知删除前的光标必为老值的光标，删除后的光标不能用新值的光标(默认没删完)
+      //     所以用老值的光标取值再去掉最后一位，可得到删除的内容*/
+      //
+      //     beginText = oldValue.text.substring(0,oldValue.selection.baseOffset);
+      //     beginText  = beginText.characters.getRange(0,beginText.characters
+      //         .length-1).string;
+      //     lastText = oldValue.text.characters.getRange(beginText.characters.length+1,oldValue.text.characters.length).string;
+      //     backText = beginText+lastText;
+      //   }catch(e){
+      //     print('--------------------------------$e');
+      // }
+      //   return TextEditingValue(
+      //       text:backText,
+      //       selection: TextSelection(
+      //         baseOffset: beginText.length,
+      //         extentOffset:  beginText.length,
+      //       ));
       }
 
     // return newValue;
