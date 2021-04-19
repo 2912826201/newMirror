a1461ffd001d38fbcfd4c683a6a1035cdbd8b059
e <111>
2021-04-19 17:54:47 +0800

    将结束直播弹出的dialog 显示时间延长

--- lib/page/training/live_broadcast/live_room_video_operation_page.dart
+++ lib/page/training/live_broadcast/live_room_video_operation_page.dart
@@ -23,6 +23,7 @@ import 'package:mirror/widget/no_blue_effect_behavior.dart';
 import 'package:mirror/widget/state_build_keyboard.dart';
 import 'package:mirror/widget/text_span_field/text_span_field.dart';
 import 'package:rongcloud_im_plugin/rongcloud_im_plugin.dart';
+import 'package:toast/toast.dart';
 
 import 'dialog/live_room_setting_dialog.dart';
 import 'dialog/live_room_online_man_number_dialog.dart';
@@ -149,7 +150,7 @@ class _LiveRoomVideoOperationPageState extends StateKeyboard<LiveRoomVideoOperat
         case 3:
         //0-直播结束
           print("直播结束");
-          ToastShow.show(msg: "直播已结束", context: context);
+          ToastShow.show(msg: "直播已结束", context: context,duration: Toast.LENGTH_LONG);
           _exitPage();
           break;
         default:
