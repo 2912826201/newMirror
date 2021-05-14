293dba9b484e2c782906f148c3e0d125d14c2581
[lianxin] <[lianxin@aimymusic.com]>
2021-05-14 12:04:38 +0800

    意见反馈先选择图片，再删除，上传的图片list其实并没有删掉，以及图片不裁剪

--- lib/widget/custom_button.dart
+++ lib/widget/custom_button.dart
@@ -10,7 +10,6 @@ import 'package:mirror/data/model/profile/black_model.dart';
 import 'package:mirror/data/notifier/profile_notifier.dart';
 import 'package:mirror/data/notifier/token_notifier.dart';
 import 'package:mirror/data/notifier/user_interactive_notifier.dart';
-import 'package:mirror/page/profile/profile_detail_page.dart';
 import 'package:mirror/route/router.dart';
 import 'package:mirror/util/toast_util.dart';
 import 'package:provider/provider.dart';
