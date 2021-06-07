a3a5e0964cf228d4f8203f2050742389a5d5b37d
Nicholas <278873964@qq.com>
2021-06-07 17:18:49 +0800

    修改上报推送id的接口路径

--- lib/api/push_api.dart
+++ lib/api/push_api.dart
@@ -5,7 +5,7 @@ import 'api.dart';
 /// push_api
 /// Created by yangjiayi on 2021/6/7.
 
-const String PUSH_UPLOADDEVICEID = "/app/web/push/uploadDeviceId";
+const String PUSH_UPLOADDEVICEID = "/appuser/web/push/uploadDeviceId";
 
 Future<bool> uploadDeviceId(String deviceId) async {
   BaseResponseModel responseModel = await requestApi(PUSH_UPLOADDEVICEID, {"deviceId": deviceId});
