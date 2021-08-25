b828f6502e9d89d7cbd173e75060ffc3c3fc1282
Nicholas <278873964@qq.com>
2021-08-25 17:45:03 +0800

    增加活动页占位页，整理tab跳转逻辑

--- lib/data/model/jump_app_page_model.dart
+++ lib/data/model/jump_app_page_model.dart
@@ -5,4 +5,7 @@ class JumpAppPageModel {
   static const int TrainingPage = 3; //训练页
   static const int MessagePage = 4; //消息页
   static const int ProfilePage = 5; //我的页面
+  static const int ActivityPage = 6; //活动页面
+
+  static const int PageSize = 6;//页面数量 在新加页面后务必修改此值
 }
