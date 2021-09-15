f14657ec5e25aa8e6f8080d214c675bfda1f0c58
e <111>
2021-09-15 18:44:17 +0800

    定位判断

--- android/app/src/main/kotlin/com/aimymusic/mirror/util/LocationServiceUtil.java
+++ android/app/src/main/kotlin/com/aimymusic/mirror/util/LocationServiceUtil.java
@@ -3,13 +3,13 @@ package com.aimymusic.mirror.util;
 import android.annotation.SuppressLint;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.location.LocationManager;
 import android.os.Build;
 import android.provider.Settings;
 import android.text.TextUtils;
 
 public class LocationServiceUtil {
 
-    private ContentResolver contentResolver;
     private Context context;
 
     @SuppressLint("StaticFieldLeak")
@@ -19,7 +19,6 @@ public class LocationServiceUtil {
         if(locationServiceUtil==null){
             locationServiceUtil = new LocationServiceUtil();
             locationServiceUtil.context = context;
-            locationServiceUtil.contentResolver = locationServiceUtil.context.getApplicationContext().getContentResolver();
         }
         return locationServiceUtil;
     }
@@ -37,19 +36,15 @@ public class LocationServiceUtil {
      * 返回定位服务开启状态
      */
     private boolean isLocationEnabled() {
-        int locationMode;
-        String locationProviders;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            try {
-                locationMode = Settings.Secure.getInt(contentResolver,Settings.Secure.LOCATION_MODE);
-            } catch (Settings.SettingNotFoundException e) {
-                e.printStackTrace();
-                return false;
-            }
-            return locationMode != Settings.Secure.LOCATION_MODE_OFF;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            // This is new method provided in API 28
+            LocationManager lm = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+            return lm.isLocationEnabled();
         } else {
-            locationProviders = Settings.Secure.getString(contentResolver, Settings.Secure.LOCATION_PROVIDERS_ALLOWED);
-            return !TextUtils.isEmpty(locationProviders);
+            // This is Deprecated in API 28
+            int mode = Settings.Secure.getInt(context.getContentResolver(), Settings.Secure.LOCATION_MODE,
+                    Settings.Secure.LOCATION_MODE_OFF);
+            return (mode != Settings.Secure.LOCATION_MODE_OFF);
         }
     }
 }
